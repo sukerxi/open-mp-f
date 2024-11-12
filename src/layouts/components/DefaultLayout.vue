539a7de1ad300c443614d03d9633a784481210d3
jxxghp <jxxghp@gmail.com>
2024-11-12 20:42:02 +0800

    Update DefaultLayout.vue to fix conditional rendering of the back button

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -64,7 +64,7 @@ onMounted(() => {
           <VIcon icon="mdi-menu" />
         </IconBtn>
         <!-- 👉 Back Button -->
-        <IconBtn v-if="appMode && display.lgAndUp" class="ms-n2" @click="goBack">
+        <IconBtn v-if="appMode && display.lgAndUp.value" class="ms-n2" @click="goBack">
           <VIcon icon="mdi-arrow-left" size="32" />
         </IconBtn>
         <!-- 👉 Search Bar -->
