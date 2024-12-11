2320c582544a13b80e206b6b53769d804a6cf247
InfinityPacer <160988576+InfinityPacer@users.noreply.github.com>
2024-12-11 19:58:33 +0800

    fix(subscribe):  update reset confirmation message

--- src/components/cards/SubscribeCard.vue
+++ src/components/cards/SubscribeCard.vue
@@ -116,7 +116,7 @@ async function resetSubscribe() {
   try {
     const isConfirmed = await createConfirm({
       title: '确认',
-      content: `重置后 ${props.media?.name} 已下载记录将被清除，未入库的剧集将会重新下载，是否确认？`,
+      content: `重置后 ${props.media?.name} 将恢复初始状态，已下载记录将被清除，未入库的剧集将会重新下载，是否确认？`,
     })
     if (!isConfirmed) return
     // 重置
