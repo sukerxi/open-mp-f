e0025889492d2c982385e3ef5d9fee3a82c4a0fc
jxxghp <jxxghp@gmail.com>
2025-04-15 21:56:51 +0800

    更新 PluginCardListView.vue

--- src/views/plugin/PluginCardListView.vue
+++ src/views/plugin/PluginCardListView.vue
@@ -625,7 +625,7 @@ onMounted(async () => {
               <template #loading />
               <template #empty />
               <div class="grid gap-4 grid-plugin-card">
-                <template v-for="data in displayUninstalledList" :key="`${data.id}_v${data.plugin_version}`">
+                <template v-for="(data, index) in displayUninstalledList" :key="`${data.id}_v${data.plugin_version}_${index}`">
                   <PluginAppCard :plugin="data" :count="PluginStatistics[data.id || '0']" @install="pluginInstalled" />
                 </template>
               </div>
